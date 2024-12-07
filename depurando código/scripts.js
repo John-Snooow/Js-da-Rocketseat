8e7138512045fa77dcaa1fccc4808887ab9106a4
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-12-06 21:38:08 -0300

    comentando no exe. de depuração de código

--- depurando código/scripts.js
+++ depurando código/scripts.js
@@ -1,13 +1,13 @@
 const input = document.querySelector("input")
 const form = document.querySelector("form")
 
-form.onsubmit =(event) =>{
-    event.preventDefault()
+form.onsubmit =(event) =>{// adiciona um evento 
+    event.preventDefault()// Previne o comportamento padrão do formulário (que seria recarregar a página)
 
-    const value = input.value
-    const hasNumberRegex = /\d+/g
+    const value = input.value// Obtém o valor digitado no input
+    const hasNumberRegex = /\d+/g// Define uma expressão regular que verifica se há números no texto
 
-    if(hasNumberRegex.test(value)){
+    if(hasNumberRegex.test(value)){// Testa se o valor contém números usando a expressão regular
         alert("O texto contém número. Por favor, digite corretamente")
     }else{
         alert("O texto está correto")
