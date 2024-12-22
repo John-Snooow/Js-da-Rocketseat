a3963ec697925128781039c7fa76d5b7db1c3950
John Frank <139596702+John-Snooow@users.noreply.github.com>
2024-12-21 21:25:52 -0300

    Criando um Array com um construtor

---
+++ Criando um array com um construtor/script.js
@@ -0,0 +1,13 @@
+//Criando Array com construtor
+
+const newArray = new Array()// cosntrutor de array
+console.log(newArray) // Saída: []
+console.log("John".length)
+console.log(newArray.length)//vai retornar 0 porque o array la en cima não tem nada 
+
+//[] - Array
+//{} - Object
+
+const available = new Array(10)//10 mostrando que tem 10 posições vazias
+console.log(available)
+console.log(available.length)// quantidadi do array
