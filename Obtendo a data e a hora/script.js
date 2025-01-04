acbbb0bc6df3e773d2718872c99451c9a8007b33
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-03 22:07:24 -0300

    Data e hora
    obtendo data e hora com Fuso horário.

---
+++ Obtendo a data e a hora/script.js
@@ -0,0 +1,9 @@
+// Exibe a data e hora atual
+
+console.log(new Date())
+
+//Exibe a data e hora de referencia (considera o fuso)
+console.log(new Date(0))
+
+//exibe o numero de milessegundos
+console.log(new Date().getTime())
