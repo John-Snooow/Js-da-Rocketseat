e4c84b698d55466588bec5f02533b484eb9541e6
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-06 22:52:00 -0300

    Metodos para trabalhar com data e horas

---
+++ Métodos pra trabalhar com data e hora/script.js
@@ -0,0 +1,24 @@
+// metodos para trabelhar com data e horas
+
+let date = new Date("2025-01-06T22:43:10")
+
+//Dia da semana de 0 à 6(domingo é 0)
+console.log(date.getDay())
+
+//Dia do mês (0 à 30)
+console.log(date.getDate())
+
+//Mês (0 à 11)
+console.log(date.getMonth())
+
+//Ano
+console.log(date.getFullYear())
+
+//Horas
+console.log(date.getHours())
+
+//Minutos
+console.log(date.getMinutes())
+
+//segundos
+console.log(date.getSeconds())
