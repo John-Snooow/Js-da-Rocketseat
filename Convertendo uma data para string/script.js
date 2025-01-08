e9623e3f04061b0241ce53f012dd4c16a4f3dd56
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-07 21:44:56 -0300

    Convertendo uma data para string

---
+++ Convertendo uma data para string/script.js
@@ -0,0 +1,10 @@
+let date = new Date("2025-01-07T14:30:00")
+
+//converte para string
+console.log(date.toString())
+
+//Retorna somente a data
+console.log(date.toDateString())
+
+//Retorna somente a hora
+console.log(date.toTimeString())
