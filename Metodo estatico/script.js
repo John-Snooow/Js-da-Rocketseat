f0ba1f68768040cc59a27c066862fdbe3615659e
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-12 17:32:06 -0300

    Metodo estatico

---
+++ Metodo estatico/script.js
@@ -0,0 +1,10 @@
+//metodos estaticos não podem ser usados coisas do construtor
+//o metodo constuctor é ultilizado quando eu instancio a class, e aqui não esta sendo instanciado nada
+
+class User {
+    static showMessage(){
+        console.log('Essa é uma mensagem!');
+    }
+}
+
+User.showMessage()
