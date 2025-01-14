149de28cdf24b4e3e7bc43677dc21f783daaf84f
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-13 22:47:55 -0300

    Criando objeto prototype

---
+++ Criando um objeto prototype/script.js
@@ -0,0 +1,16 @@
+//Usandp prototype com:
+//prototype é um mecanismo fundamental que permite a herança de propriedades e métodos entre objetos.
+
+const address = {//objeto
+  city: "São Paulo",
+  coubtry: "Brasil",
+}
+
+console.log(address)
+
+const users = ["Rodrigo", "John", "Maria"]//Array
+
+console.log(users)
+
+const userName = "Rodrigo"//string
+console.log(userName.__proto__)
