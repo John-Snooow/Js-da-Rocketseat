4688f9f04cbc29de93e8981bbd0c5034c37783d6
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-14 23:44:32 -0300

    comentando o codigo

--- Como ultilizar classes pra lidar com exeções/script.js
+++ Como ultilizar classes pra lidar com exeções/script.js
@@ -1,24 +1,36 @@
+// Defina um array com um único elemento, o número 17
 let obj = [17]
+
+// Defina uma variável index com o valor 300
 let index = 300
-try{
+
+// Bloco try-catch para lidar com exceções e erros
+try {
+  // Essa linha está comentada, mas se fosse executada, lançaria uma exceção TypeError
+  // porque o array obj não tem um método chamado execute()
   // obj.execute()
 
-  if(!obj.includes(17)){
-    throw new Error("o número 17 não esta disponivel")
+  // Verifique se o número 17 está incluído no array obj
+  if (!obj.includes(17)) {
+    // Se o número 17 não estiver incluído, lance uma exceção do tipo Error
+    throw new Error("o número 17 não está disponível")
   }
 
-  if(index>99){
-    throw new RangeError(
-      "Numero esta fora do intervalo. Escolha um número de 0 à 99."
-    )
+  // Verifique se o valor de index é maior que 99
+  if (index > 99) {
+    // Se o valor de index for maior que 99, lance uma exceção do tipo RangeError
+    throw new RangeError("Número está fora do intervalo. Escolha um número de 0 à 99.")
   }
-}catch(error){
-  if (error instanceof TypeError){
-    console.log("Metodo indisponivel.")
-  }
-
-  if(error instanceof RangeError){
+} catch (error) {
+  // Verifique o tipo de exceção lançada
+  if (error instanceof TypeError) {
+    // Se a exceção for do tipo TypeError, imprima uma mensagem de erro
+    console.log("Método indisponível.")
+  } else if (error instanceof RangeError) {
+    // Se a exceção for do tipo RangeError, imprima a mensagem de erro
     console.log(error.message)
+  } else {
+    // Se a exceção for de outro tipo, imprima a exceção
+    console.log(error)
   }
-  // console.log(error)
 }
