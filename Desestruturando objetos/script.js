f46745f1306be4c153dc7481e6cf1249afe78055
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-25 18:02:26 -0300

    Desestruturando objetos

---
+++ Desestruturando objetos/script.js
@@ -0,0 +1,21 @@
+//destruturing assinment (desestrturação) permite extrair dados de arrays e objetos de variaveis distintas
+
+const product ={
+    description: "teclado",
+    price: 100.00,
+}
+
+const {description, price} = product
+console.log("Descrição: ", description)
+console.log("Preço: ", price)
+
+function newProduct ({price, description}){
+    console.log("## NOVO PREÇO ##")
+    console.log("Descrição: ", description)
+    console.log("preço: R$", price)
+}
+
+newProduct({
+    description: "mouse",
+    price: 100,
+})
