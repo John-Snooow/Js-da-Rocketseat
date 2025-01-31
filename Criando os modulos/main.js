d6fcc529cfca8c3ca4ad619edf5b19b34c2efee8
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-01-31 20:59:25 -0300

    importando tudo

--- Criando os modulos/main.js
+++ Criando os modulos/main.js
@@ -1,4 +1,13 @@
+/*
+aqui eu estou fazendo um importação separadamento 
 import { sum, multiply} from "./calc.js"
 
 console.log(" 4 + 6 =", sum(4, 6))
 console.log(" 4 * 6 =", multiply(4, 6))
+*/
+
+//aqui eu estou fazendo um importação de tudo 
+import * as calc from "./calc.js"
+
+console.log(" 4 + 6 =", calc.sum(4, 6))
+console.log(" 4 * 6 =", calc.multiply(4, 6))
