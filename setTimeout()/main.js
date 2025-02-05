a15fabbf4cd2f9ff5bc0c88ddf4ccd0eb5c0a9eb
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-02-04 21:59:44 -0300

    conhecendo setTimeout()

---
+++ setTimeout()/main.js
@@ -0,0 +1,13 @@
+// setTimeout() executa um intervalo de tempo após um intervalo de tempo especificado.
+
+setTimeout(() => {
+    console.log('Hello, World!'); // imprime 'Hello, World!' após 2
+}, 3000) // tempo em milissegundos 3s.
+
+setTimeout(() => {
+    console.log('Bora'); 
+}, 4000)
+
+setTimeout(() => {
+    console.log('Codar?'); 
+}, 5000)
